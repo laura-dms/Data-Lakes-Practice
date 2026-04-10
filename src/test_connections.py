94d0b13150a95cd9f506b96e37fd9eb9df0b8776
laura.dms <laura.damas@efrei.net>
2026-04-10 12:02:11 +0200

    updates tp3

--- src/test_connections.py
+++ src/test_connections.py
@@ -12,7 +12,7 @@ def test_mysql():
     try:
         conn = mysql.connector.connect(
             host="localhost", user="root",
-            password="root", database="staging", port=3307
+            password="root", database="staging"
         )
         cursor = conn.cursor()
         cursor.execute("SELECT 1")
